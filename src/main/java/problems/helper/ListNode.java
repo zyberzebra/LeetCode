2bfe5eb33975770789712aa450e2165f0bd10104
zyberzebra <zyberzebra@users.noreply.github.com>
2024-03-23 12:54:06 +0100

    feat: add to string and creation helper

--- src/main/java/problems/helper/ListNode.java
+++ src/main/java/problems/helper/ListNode.java
@@ -1,5 +1,12 @@
 package problems.helper;
 
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+
+@NoArgsConstructor
 public class ListNode {
     public int val;
     public ListNode next;
@@ -13,8 +20,42 @@ public class ListNode {
         this.next = next;
     }
 
+    public static ListNode createListNode(List<Integer> numbers) {
+        List<ListNode> nodes = new LinkedList<>();
+        for (int number : numbers) {
+                nodes.add(new ListNode(number));
+        }
+        if (numbers.size() == 1) {
+            return nodes.getFirst();
+        }
+        ListNode iter = new ListNode();
+        ListNode pointer = new ListNode(numbers.getFirst(),iter);
+        pointer.next = iter;
+        for (int i = 1; i < numbers.size(); i++) {
+            iter.val = numbers.get(i);
+            if (i == numbers.size() - 1 ){
+                break;
+            }
+            iter.next = new ListNode();
+            iter = iter.next;
+        }
+        return pointer;
+    }
+
+
     @Override
     public String toString() {
-        return next != null ? val + next.toString() : String.valueOf(val);
+        StringBuilder sb = new StringBuilder();
+        ListNode current = this;
+        sb.append("[");
+        while (current != null) {
+            sb.append(current.val);
+            if (current.next != null) {
+                sb.append("");
+            }
+            current = current.next;
+        }
+        sb.append("]");
+        return sb.toString();
     }
 }
