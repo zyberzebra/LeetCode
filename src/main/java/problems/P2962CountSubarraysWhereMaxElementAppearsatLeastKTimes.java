22269ba38cc1a144b51dc0c7de3ccc17a5e4b7f8
ZybrZebra <zyberzebra@users.noreply.github.com>
2024-03-29 14:00:23 +0100

    problem(2962): make algo faster

--- src/main/java/problems/P2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
+++ src/main/java/problems/P2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
@@ -6,28 +6,25 @@ import java.util.Map;
 
 /**
  * <a href="https://leetcode.com/count-subarrays-where-max-element-appears-at-least-k-times">
- *     count-subarrays-where-max-element-appears-at-least-k-times</a>
+ * count-subarrays-where-max-element-appears-at-least-k-times</a>
  */
 public class P2962CountSubarraysWhereMaxElementAppearsatLeastKTimes {
-   static long countSubarrays(int[] nums, int k) {
+    static long countSubarrays(int[] nums, int k) {
         int left = 0;
-        int right = 0;
-        int count = 0;
-       int max = Arrays.stream(nums).max().orElse(0);
-       Map<Integer, Integer> longLongMap = new HashMap<>();
-       longLongMap.put(max, 0);
-       while (left < nums.length) {
-           while (right < nums.length) {
-               longLongMap.put(nums[right], longLongMap.getOrDefault(nums[right], 0) + 1);
-               if (longLongMap.getOrDefault(max,0) >= k) {
-                   count++;
-               }
-               right++;
-           }
-           left++;
-           right = left;
-           longLongMap.clear();
-       }
-       return count;
+        long count = 0;
+        long max = Arrays.stream(nums).max().orElse(0);
+        Map<Long, Long> longLongMap = new HashMap<>();
+        longLongMap.put(max, 0L);
+
+        for (int right = 0; right < nums.length; right++) {
+            longLongMap.put((long)nums[right], longLongMap.getOrDefault((long)nums[right], 0L) + 1);
+
+            while (left <= right && longLongMap.getOrDefault(max, 0L) >= k) {
+                count += nums.length - right;
+                longLongMap.put((long)nums[left], longLongMap.get((long)nums[left]) - 1);
+                left++;
+            }
+        }
+        return count;
     }
 }
