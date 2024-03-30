0b1619ce4af3ffeabc26e15a10dafab4cd64bef2
ZybrZebra <zyberzebra@users.noreply.github.com>
2024-03-30 15:39:45 +0100

    problem(2962): provide big case
    bigger than int

--- src/test/java/problems/P2962CountSubarraysWhereMaxElementAppearsatLeastKTimesTest.java
+++ src/test/java/problems/P2962CountSubarraysWhereMaxElementAppearsatLeastKTimesTest.java
@@ -1,25 +1,32 @@
 package problems;
 
+import com.google.common.io.Files;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 class P2962CountSubarraysWhereMaxElementAppearsatLeastKTimesTest {
-    static Stream<Arguments> provideTestData() {
+    static Stream<Arguments> provideTestData() throws IOException {
         return Stream.of(
                 arguments(new int[]{1,3,2,3,3}, 2, 6),
-                arguments(new int[]{1,4,2,1}, 3, 0)
+                arguments(new int[]{1,4,2,1}, 3, 0),
+                arguments(Arrays.stream(Files.readFirstLine(new File("src/test/resources/P2962Testdata"), Charset.defaultCharset()).split(",")).mapToInt(Integer::valueOf).toArray()
+                ,6282,2815068166L)
         );
     }
 
     @ParameterizedTest
     @MethodSource("provideTestData")
-    void maxSubarrayLength(int[] nums, int k, int expected) {
+    void maxSubarrayLength(int[] nums, int k, long expected) {
         assertThat(P2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.countSubarrays(nums, k)).isEqualTo(expected);
     }
 }
